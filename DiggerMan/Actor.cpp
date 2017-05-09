48c0a73ca7edddea2c858589746727ffb99ddcbc
Nick Schneider <nickschneider101@gmail.com>
2017-05-09 12:43:40 -0700

    Moved StudentWorld to .cpp and Added ability for boulders to wait and fall

--- DiggerMan/Actor.cpp
+++ DiggerMan/Actor.cpp
@@ -10,93 +10,135 @@ void Actor::doSomething()
 
 void DiggerMan::doSomething()
 {
-	cout << "X: " << getX() << "| Y: " << getY() << endl;
-	StudentWorld* world = getWorld();
-	
-	int numValue = 0;
-	
-	//////////MOVE HANDLER//////////
-	if (world->getKey(numValue))
-	{
-		if (numValue == KEY_PRESS_LEFT)
-		{
-			if (getDirection() != left)
-			{
-				setDirection(left);
-			}
+    //cout << "X: " << getX() << "| Y: " << getY() << endl;
+    StudentWorld* world = getWorld();
 
-			else if (getX() >= 1 && getDirection() == left)
-			{
-				moveTo(getX() - 1, getY());
-				world->deleteDirt(getX(), getY());
-				setDirection(left);
-			}
-		}
-		if (numValue == KEY_PRESS_UP)
-		{
-			if (getDirection() != up)
-			{
-				setDirection(up);
-			}
-			
-			else if (getY() <= MAXSIZE_Y - 5 && getDirection() == up)
-			{
-				moveTo(getX(), getY() + 1);
-				world->deleteDirt(getX(), getY());
-				setDirection(up);
-			}
-		}
-		if (numValue == KEY_PRESS_RIGHT)
-		{
-			if (getDirection() != right)
-			{
-				setDirection(right);
-			}
-			
-			else if (getX() <= MAXSIZE_X - 5 && getDirection() == right)
-			{
-				moveTo(getX() + 1, getY());				
-				world->deleteDirt(getX(), getY());
-				setDirection(right);
-			}
-		}
-		if (numValue == KEY_PRESS_DOWN)
-		{
-			if (getDirection() != down)
-			{
-				setDirection(down);
-			}
-			
-			else if (getY() >= 1 && getDirection() == down)
-			{
-				moveTo(getX(), getY() - 1);
-				world->deleteDirt(getX(), getY());
-				setDirection(down);
-			}
-			
-		}
-	}
+    int numValue = 0;
+
+    //////////MOVE HANDLER//////////
+    if (world->getKey(numValue))
+    {
+        if (numValue == KEY_PRESS_LEFT)
+        {
+            if (getDirection() != left)
+            {
+                setDirection(left);
+            }
+
+            else if (getX() >= 1 && getDirection() == left)
+            {
+                moveTo(getX() - 1, getY());
+                world->deleteDirt(getX(), getY());
+                setDirection(left);
+            }
+        }
+        if (numValue == KEY_PRESS_UP)
+        {
+            if (getDirection() != up)
+            {
+                setDirection(up);
+            }
+
+            else if (getY() <= MAXSIZE_Y - 5 && getDirection() == up)
+            {
+                moveTo(getX(), getY() + 1);
+                world->deleteDirt(getX(), getY());
+                setDirection(up);
+            }
+        }
+        if (numValue == KEY_PRESS_RIGHT)
+        {
+            if (getDirection() != right)
+            {
+                setDirection(right);
+            }
+
+            else if (getX() <= MAXSIZE_X - 5 && getDirection() == right)
+            {
+                moveTo(getX() + 1, getY());
+                world->deleteDirt(getX(), getY());
+                setDirection(right);
+            }
+        }
+        if (numValue == KEY_PRESS_DOWN)
+        {
+            if (getDirection() != down)
+            {
+                setDirection(down);
+            }
+
+            else if (getY() >= 1 && getDirection() == down)
+            {
+                moveTo(getX(), getY() - 1);
+                world->deleteDirt(getX(), getY());
+                setDirection(down);
+            }
+
+        }
+    }
 }
 
 bool Boulder::isStable()
-{	
-	StudentWorld* world = getWorld();
-	return world->checkDirtBelow(getX(), getY());
+{
+    StudentWorld* world = getWorld();
+    //return world->checkDirtBelow(getX(), getY());
+    return world->checkActorBelow(getX(), getY(),IMID_DIRT);
 }
 
 void Boulder::doSomething()
 {
-	//TODO: Check if still alive and immediately return
-	if (isStable())
-	{
-		m_state = stable;
-		cout << "\tBoulder at " << getX() << "|" << getY() << " stable\n";
-	}
-	else
-	{
-		m_state = waiting;
-		cout << "\tBoulder at " << getX() << "|" << getY() << " NOT stable\n";
+    //TODO: Check if still alive and immediately return
+    if (!alive())
+    {
+        cout << "\tBoulder at " << getX() << "|" << getY() << " is dead\n";
+        return;
+    }
 
-	}
+    if (m_state != waiting && m_state != falling) //If boulder is in normal state
+    {
+        if (isStable())
+        {
+            m_state = stable;
+            cout << "\tBoulder at " << getX() << "|" << getY() << " is stable\n";
+        }
+        else
+        {
+            m_state = waiting;
+            cout << "\tBoulder at " << getX() << "|" << getY() << " is waiting\n";
+        }
+        cout << "-----------------------------------------\n";
+    }
+    else if (waitTime < 30 && m_state == waiting)
+    {
+        waitTime++;
+        cout << "\tBoulder at " << getX() << "|" << getY() << " is waiting for " << waitTime << " ticks\n";
+        return;
+    }
+    else if (waitTime >= 30 && m_state == waiting)
+    {
+        waitTime = 0;
+        m_state = falling;
+        cout << "\tBoulder at " << getX() << "|" << getY() << " is moving to a falling state\n";
+        getWorld()->playSound(SOUND_FALLING_ROCK);
 
+    }
+    if (m_state == falling)
+    {
+        //Move down one square each tick until it hits the bottom of the field
+        //Runs on top of another boulder
+        //Runs into dirt (By moving down a square the boulder would overlap
+        //Then must set state to dead so it can be removed from game
+        cout << "\tBoulder at " << getX() << "|" << getY() << " is falling\n";
+
+        if (getY() >= 1 && !getWorld()->checkActorBelow(getX(), getY(),IMID_DIRT))
+        {
+            moveTo(getX(), (getY() - 1));
+        }
+        else
+        {
+            m_state = stable;
+            setHitpoints(0);
+            cout << "\tBoulder at " << getX() << "|" << getY() << " is dead\n";
+        }
+    }
 }
--- DiggerMan/Actor.h
+++ DiggerMan/Actor.h
@@ -11,58 +11,63 @@ class StudentWorld; //Forward declaration
 
 enum BoulderState
 {
-	stable, falling, waiting
+    stable, falling, waiting
 };
 
 class Actor : public GraphObject
 {
 public:
-	Actor(StudentWorld * world, int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
-	:GraphObject(imageID, startX, startY, right, size, depth)
-	{
-		m_world = world;
-	}
+    Actor(StudentWorld * world, int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
+        :GraphObject(imageID, startX, startY, right, size, depth)
+    {
+        m_hitpoints = 1;
+        m_world = world;
+    }
 
-	inline void setHitpoints(int newHitpoints)
-	{
-		this->m_hitpoints = newHitpoints;
-	}
-	inline int getHitpoints()
-	{
-		return m_hitpoints;
-	}
-	virtual StudentWorld* getWorld()
-	{
-		return m_world;
-	}
+    inline void setHitpoints(int newHitpoints)
+    {
+        this->m_hitpoints = newHitpoints;
+    }
+    inline int getHitpoints()
+    {
+        return m_hitpoints;
+    }
+    virtual bool alive()
+    {
+        return m_hitpoints > 0;
+    }
+    virtual StudentWorld* getWorld()
+    {
+        return m_world;
+    }
 
-	virtual void doSomething();
+    virtual void doSomething();
 
 private:
-	int m_hitpoints;
-	StudentWorld * m_world;
+    int m_hitpoints;
+    StudentWorld * m_world;
 };
 
 class DiggerMan : public Actor
 {
 public:
-	DiggerMan(StudentWorld * world)
-		: Actor(world, IMID_PLAYER, DM_START_X, DM_START_Y)
-	{
-		setVisible(true);
-	}
+    DiggerMan(StudentWorld * world)
+        : Actor(world, IMID_PLAYER, DM_START_X, DM_START_Y)
+    {
+        setVisible(true);
+    }
 
-	virtual void doSomething();
-	
-	virtual StudentWorld* getWorld()
-	{
-		return Actor::getWorld();
-	}
+    virtual void doSomething();
+
+    virtual StudentWorld* getWorld()
+    {
+        return Actor::getWorld();
+    }
 
 private:
-	int m_water;
-	int m_sonarCharges;
-	int m_goldNuggets;
+    int m_water;
+    int m_sonarCharges;
+    int m_goldNuggets;
 };
 
 class Squirt : public Actor
@@ -71,11 +76,11 @@ class Squirt : public Actor
 class Dirt : public Actor
 {
 public:
-	Dirt(StudentWorld * world, int startX, int startY)
-		: Actor(world, IMID_DIRT, startX, startY, right, 0.25, 3)
-	{
-		setVisible(true);
-	}
+    Dirt(StudentWorld * world, int startX, int startY)
+        : Actor(world, IMID_DIRT, startX, startY, right, 0.25, 3)
+    {
+        setVisible(true);
+    }
 
 private:
 
@@ -84,45 +89,44 @@ private:
 class Barrel : public Actor
 {
 public:
-	Barrel(StudentWorld * world, int startX, int startY)
-		: Actor(world, IMID_BARREL, startX, startY, right, 1.0, 2)
-	{
-		setVisible(false); //Barrels should start hidden and only be discovered when walked over
-	}
+    Barrel(StudentWorld * world, int startX, int startY)
+        : Actor(world, IMID_BARREL, startX, startY, right, 1.0, 2)
+    {
+        setVisible(false); //Barrels should start hidden and only be discovered when walked over
+    }
 };
 
 class GoldNugget : public Actor
 {
 public:
-	GoldNugget(StudentWorld * world, int startX, int startY)
-		: Actor(world, IMID_GOLD, startX, startY, right, 1.0, 2)
-	{
-		setVisible(true);
-	}
+    GoldNugget(StudentWorld * world, int startX, int startY)
+        : Actor(world, IMID_GOLD, startX, startY, right, 1.0, 2)
+    {
+        setVisible(true);
+    }
 };
 
 class Boulder : public Actor
 {
-	//Must start in a stable state, must add code later
+    //Must start in a stable state, must add code later
 public:
-	Boulder(StudentWorld * world, int startX, int startY)
-		:Actor(world, IMID_BOULDER, startX, startY, down, 1.0, 1)
-	{
-		this->m_state = stable;
-		setVisible(true);
-	}
+    Boulder(StudentWorld * world, int startX, int startY)
+        :Actor(world, IMID_BOULDER, startX, startY, down, 1.0, 1)
+    {
+        this->m_state = stable;
+        setVisible(true);
+    }
 
-	void doSomething();
-	bool isStable();
-	BoulderState getState()
-	{
-		return m_state;
-	}
-	
-	
+    void doSomething();
+    bool isStable();
+    BoulderState getState()
+    {
+        return m_state;
+    }
 
 private:
-	BoulderState m_state;
+    BoulderState m_state;
+    unsigned int waitTime = 0;
 };
 
 class Protester : public Actor
